88d4e4198c1ba2681d810379cbfec54f150e4f22
Manoj-Nagireddy <manoj.nagireddy999@gmail.com>
2024-11-02 00:29:36 +0530

    adding utils & init scripts

--- synthetic_data_generation/Synthetic_data_generate.py
+++ synthetic_data_generation/Synthetic_data_generate.py
@@ -1,22 +1,9 @@
-import faker
+import os, faker
 from faker.providers import BaseProvider
 import re
 import pandas as pd
 from streamlit import selectbox, text_input, button, write
-
-# **Schema Dictionary with Regex Patterns**
-schema = {
-    "first_name": {"faker": "first_name", "regex": r"^[A-Za-z ]+$"},
-    "last_name": {"faker": "last_name", "regex": r"^[A-Za-z ]+$"},
-    "address": {"faker": "address", "regex": r"^[A-Za-z0-9\s,.-]+$"},
-    "HCID": {"generator": lambda: faker.Faker().numerify("##########"), "regex": r"^\d{9}$"},
-    "UM_id": {"generator": lambda: faker.Faker().numerify("########"), "regex": r"^\d{8}$"},
-    "DCN": {"generator": lambda: faker.Faker().numerify("##########"), "regex": r"^\d{9}$"},
-    "policy number": {"generator": lambda: faker.Faker().numerify("######-####"), "regex": r"^\d{6}-\d{4}$"},
-    "claim amount": {"generator": lambda: round(faker.Faker().random.uniform(100.0, 5000.0), 2), "regex": r"^\d+(\.\d{2})?$"},
-    "ICD-10 codes": {"generator": lambda: faker.Faker().random_element(elements=("S06.0X0A", "I10", "Z91.81")), "regex": r"^[A-Z][0-9].[0-9]{1}([A-Z])?$"},
-    "CPT codes": {"generator": lambda: faker.Faker().random_element(elements=("99213", "77067", "36415-59")), "regex": r"^\d{5}(?:-\d{1,2})?$"}
-}
+from utils import *
 
 # **Faker Instance**
 fake = faker.Faker()
